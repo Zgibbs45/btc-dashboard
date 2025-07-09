114937f15e53108b473578a9540c5d69babbcf6a
Zgibbs45 <zgibbs@cleanspark.com>
2025-07-09 10:16:09 -0700

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -139,7 +139,7 @@ range_options = {
     "1 Day": "1d", "1 Week": "5d", "1 Month": "1mo", "6 Months": "6mo", "1 Year": "1y"
 }
 
-competitor_tickers = ["BITF", "CANG", "CIFR", "CORZ", "HIVE", "HUT", "IREN", "MARA", "MTPLF", "RIOT", "WULF"]
+competitor_tickers = ["BITF", "CANG", "CIFR", "CORZ", "HIVE", "HUT", "IREN", "MARA", "MTPLF", "RIOT", "WULF", "CLSK"]
 
 # --- Helper Functions ---
 
