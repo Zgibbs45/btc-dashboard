2ff89b738870b3bc3d4e535273c554497fb72e74
Zgibbs45 <zgibbs@cleanspark.com>
2025-07-10 16:27:29 -0700

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -1075,6 +1075,7 @@ if tab == "Live Market":
 
                     st.altair_chart(stock_chart, use_container_width=True)
                     st.write("Last candle timestamp in Eastern Time:", df.index.max())
+                    st.write("Data preview:", stock_close.tail(10))
 
                 else:
                     st.warning("No price data available for this range.")
