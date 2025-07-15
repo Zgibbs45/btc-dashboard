c47034ddfec0cd844187de78a2941da034aa1670
Zgibbs45 <zgibbs@cleanspark.com>
2025-07-15 11:07:28 -0700

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -1200,11 +1200,11 @@ if tab == "Live Market":
 
         # Format time for tooltip
         if comp_selected_period == "1d":
-            chart_df["TimePST"] = chart_df["Date"].dt.strftime("%H:%M %p"),
+            chart_df["TimePST"] = chart_df["Date"].dt.strftime("%H:%M %p")
             time_title = "Time (PST)"
         else:
-            chart_df["TimePST"] = chart_df["Date"].dt.strftime("%b %d"),
-            time_title = "Date"
+            chart_df["TimePST"] = chart_df["Date"].dt.strftime("%b %d")
+            date_title = "Date"
         chart_df["Price"] = pd.to_numeric(chart_df["Price"], errors="coerce")
         chart_df.dropna(subset=["Price"], inplace=True)
         chart_df["Price"] = chart_df["Price"].round(2)
