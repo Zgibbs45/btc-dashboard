cb5dfd9ae810a0c7799caa9cfd079bf8d9ec2366
Zgibbs45 <zgibbs@cleanspark.com>
2025-07-21 10:37:41 -0700

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -777,7 +777,8 @@ if tab == "Bitcoin News":
         btc_close["Date"] = pd.to_datetime(btc_close["Date"])
         if btc_close["Date"].dt.tz is None:
             btc_close["Date"] = btc_close["Date"].dt.tz_localize("UTC")
-        btc_close["Date"] = btc_close["Date"].dt.tz_convert("US/Eastern")
+        btc_close["Date_PST"] = btc_close["Date"].dt.tz_convert("US/Pacific")
+        btc_close["Label"] = btc_close["Date_PST"].dt.strftime("%-I:%M %p")
         if selected_range == "1d":
             btc_close.set_index("Date", inplace=True)
             btc_close = btc_close.resample("30min").first().dropna().reset_index()
